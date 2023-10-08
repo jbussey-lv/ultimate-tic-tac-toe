5422999368237a0fb6003d0d11fbc65b0f036995
John Bussey <john.bussey@enernoc.com>
2023-10-08 15:11:33 -0400

    got rid of unused controller methods

--- app.py
+++ app.py
@@ -13,14 +13,6 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 def get_game_link(game_key, player):
     return "/games/" + game_key + "/" + player
 
-def extract_game_key(referrer):
-    sub1 = "games/"
-    sub2 = "/"
-    idx1 = referrer.find(sub1)
-    end = referrer[idx1 + len(sub1):]
-    idx2 = referrer.find(sub2)
-    return end[0: idx2]
-
 @socketio.event
 def connect():
     game_key = request.args['gameKey']
@@ -68,16 +60,6 @@ def get_game(game_key, this_player):
     return render_template('game.html', game_key=game_key, this_player=this_player, **full_data)
 
 
-@app.route('/api/games/<string:game_key>', methods=['GET'])
-def api_game_data(game_key):
-    try:
-        game = GameManager.retrieve_game(game_key)
-    except:
-        return {"error": f'no game found for key {game_key}'}
-    
-    return game.get_full_data()
-
-
 @app.route('/games', methods=['GET'])
 def join_game():
     key = request.args.get('game_key')
@@ -85,41 +67,6 @@ def join_game():
     player = game.players[1]
     game_link = get_game_link(key, player)
     return redirect(game_link)
-
-@app.route('/games/<string:game_key>/reset', methods=['POST'])
-def reset_game(game_key):
-
-    try:
-        game = GameManager.retrieve_game(game_key)
-        game.reset()
-    except:
-        redirect ('/')
-    
-    GameManager.save_game(game_key, game)
-
-    socketio.emit('game_data', game.get_full_data(), room=game_key)
-
-    return redirect(request.referrer)
-
-@app.route('/games/<string:game_key>/moves', methods=['POST'])
-def moves(game_key):
-    try:
-        game = GameManager.retrieve_game(game_key)
-    except:
-        redirect ('/')
-
-    big_row_index = int(request.form.get('big_row_index'))
-    big_col_index = int(request.form.get('big_col_index'))
-    small_row_index = int(request.form.get('small_row_index'))
-    small_col_index = int(request.form.get('small_col_index'))
-    game.add_move(big_row_index, big_col_index, small_row_index, small_col_index)
-
-    GameManager.save_game(game_key, game)
-
-    socketio.emit('message', 'refresh', room=game_key)
-
-    return redirect(request.referrer)
-
  
 # main driver function
 if __name__ == '__main__':
